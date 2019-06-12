9f6129a438bdd6ec41c19930888e4bf71d4f07bc
teleserv <yangxiaoshi2002@gmail.com>
2019-06-12 10:24:31 -0400

    Create run.py

---
+++ week03/hw/image/tx2/forwarder/run.py
@@ -0,0 +1,25 @@
+import paho.mqtt.client as mqtt
+
+# This is the Subscriber
+
+def on_connect(client, userdata, flags, a):
+  print("Connected with result code "+str(a))
+  client.subscribe("_tx2/webcam/#", qos=0)
+
+def on_message(client, userdata, msg):
+  client_pub.publish(msg.topic, msg.payload, qos=1)
+  print ("message is published")
+
+while True:
+  try:
+    client_sub = mqtt.Client()
+    client_sub.connect("mosquitto",1883, 10)
+
+    client_pub = mqtt.Client()
+    client_pub.connect("169.61.86.171",1883, 20)
+
+    client_sub.on_connect = on_connect
+    client_sub.on_message = on_message
+    
+  except:
+    continue
