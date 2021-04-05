15922a79d87e13d65777cb091069cbd297d5bdb6
Darragh Hanley <darragh.hanley@gmail.com>
2021-04-05 16:11:23 +0100

    Rnn on sine wav

--- week13/labs/ts_demo/rnn.py
+++ week13/labs/ts_demo/rnn.py
@@ -1,88 +1,141 @@
 import pandas as pd
 import numpy as np
+import random
 import torch
 from torch import nn
-# Load the sequence data
-url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series'
-confdf = pd.read_csv( f'{url}/time_series_covid19_confirmed_global.csv' )
+random.seed(0)
 
-confdf.iloc[0]
-
-confdf = confdf.drop(['Province/State',  'Lat', 'Long'], 1).set_index('Country/Region')
-confdf
-
-confdf.loc['Afghanistan'].plot()
-confdf.loc['Italy'].plot()
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows',1000)
+pd.set_option('display.width', 1000)
+pd.set_option('display.float_format', lambda x: '%.3f' % x)
 
 '''
-Normalisation
+Continuous Sequence data
 '''
-# Lets look at the distribution of our data
-pd.Series(confdf.values.flatten()).hist(bins = 100)
+# Single sine wave sequence
+def sinfn(f, slen, bias):
+    return np.sin(np.linspace(-f, f, slen)) + bias
 
-pd.Series(np.log(1+ confdf.values.flatten())).hist(bins = 100)
+x  = sinfn(np.pi, 1000, 5)
+pd.DataFrame(x).plot()
 
-pd.Series(np.log(1+ confdf.values.flatten()) /20 ).hist(bins = 100)
+# Multiple sequences of different length
+data = []
+for i in range(8):
+    f, slen, bias = random.uniform(2, 4), random.randint(100, 200), random.uniform(0, 5)
+    x = torch.tensor(sinfn(f, slen, bias) + np.random.randn(slen)*0.1)
+    x = x.unsqueeze(1).float()
+    data.append(x)
 
-# so lets log norm it and divide by 20
-def normfn(m):
-    return np.log(1+m) / 20
-def revnormfn(m):
-        return np.exp(20 * m) - 1
+# We have multiple lengths with one column in each sequence
+[i.shape for i in data]
 
-confdf.values
-normfn(confdf.values)
-revnormfn(normfn(confdf.values))
+# Let's pad the sequences so they all get the same length
+from torch.nn.utils.rnn import pad_sequence
+PADVALUE = -1
+batch = pad_sequence(data, batch_first = True, padding_value=PADVALUE)
 
-'''
-Transform data
-'''
-data = torch.tensor(normfn(confdf.values)).float() 
-data.shape
-data = data.unsqueeze(2)
-data.shape
-data = data.permute(0,2,1)
-data.shape
-
-trndata = data[:,:,:-5]
-tstdata = data[:,:,-5:]
+batch.shape
+batch[:,:1]
+batch[:,-1:]
 
 
-'''
-Prototype Modelling
-'''
+batchdf = batch.squeeze(-1).numpy().transpose()
+pd.DataFrame(batchdf[::-1]).plot()
 
-x = data[:4]
+
+# Create a mask so we can tell the model where our padding is
+mask = batch == -1
+pd.DataFrame(mask[:,:,0].numpy().transpose())
+
+# First lets normalise our data
+dmean = batch[~mask].mean()
+dstd = batch[~mask].std()
+
+batch[~mask] = (batch[~mask] - dmean) / dstd
+
+batchdf = batch.squeeze(-1).numpy().transpose()
+pd.DataFrame(batchdf[::-1]).plot()
+
+# Split the data into our target and actuals
+yact = batch[:, :5]
+x = batch[:, 5:]
+
+
+
+# It's also good to apply batchnorm on the sequence data batches as the model trains. 
+# https://pytorch.org/docs/stable/generated/torch.nn.BatchNorm1d.html
+bn0 = nn.BatchNorm1d(num_features=batch.size(-1))
+
+bn0
 x.shape
-# Lets create a class to store our model. 
-bn0 = nn.BatchNorm1d(num_features=1)
+x.permute(0 ,2, 1).shape
 
-x = bn0(x)
+x = bn0(x.permute(0 ,2, 1))
 x.shape
 
-x = x.permute(0,2,1)
-x.shape
-
-hidden = 32
-rnn = nn.LSTM(1, hidden)
-
-rnn(x)
+x = x.permute(0 ,2, 1)
 
 
+# Now let's try an RNN on the sequence data. 
+#
+hidden_dim = 32
+# https://pytorch.org/docs/stable/generated/torch.nn.LSTM.html
+lstm = nn.LSTM(input_size=1,hidden_size=hidden_dim,batch_first=True, bidirectional = False)
+lstm
+fc = nn.Linear(in_features=hidden_dim ,out_features=1)
+fc
+
+criterion = nn.MSELoss()
+
+
+hidden, (h0, c0) = lstm(x)
+hidden.shape, h0.shape, c0.shape
+
+ypred = fc(hidden)
+ypred.shape
+
+# Let's get the loss
+criterion = nn.MSELoss(reduction = 'none')
+loss = criterion(ypred[:, :5], yact)
+loss
+
+criterion = nn.MSELoss()
+loss = criterion(ypred[:, :5], yact)
+loss
+
+
+# Bring it all together 
 
 class TimeSeriesRNN(nn.Module):
-    def __init__(self, hidden = 32):
+    def __init__(self, input_feats, outlen, hidden = 32):
         super().__init__()
-        self.rnn = nn.LSTM(IN_UNITS, 32)
-        self.n_features = self.cnn.fc.in_features
-        self.cnn.global_pool = nn.Identity()
-        self.cnn.fc = nn.Identity()
+        self.rnn = nn.LSTM(input_size=input_feats,
+                           hidden_size=hidden_dim,
+                           batch_first=True, 
+                           bidirectional = False)
+        self.fc = nn.Linear(in_features=hidden_dim ,out_features=1)
+        self.bn = nn.BatchNorm1d(num_features=input_feats)
+        self.outlen = outlen
 
-    def forward(self, x):
-        bs = x.size(0)
-        features = self.cnn(x)
-        features = features.permute(0, 2, 3, 1)
-        return features
+    def forward(self, xinp):
+        # xinp = batch
+        x = xinp.permute(0 ,2, 1)
+        x =  self.bn(x)
+        x = x.permute(0 ,2, 1)
+        
+        hidden, _ = self.rnn(x)
+        hidden = hidden[:,:self.outlen]
+        
+        out = self.fc(hidden)
+        
+        return out
 
+# self = model = TimeSeriesRNN(input_feats = 1, outlen = yact.size(1), hidden = 32)
+model = TimeSeriesRNN(input_feats = 1, outlen = yact.size(1), hidden = 32)
+model
 
-
+ypred = model(batch)
+loss = criterion(ypred, yact)
+loss
