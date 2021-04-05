1bbcdaa1e9d8e68a3e91d4eb36554d4b538e57e9
Darragh Hanley <darragh.hanley@gmail.com>
2021-04-05 16:52:20 +0100

    Rnn on sine wav

--- week13/labs/ts_demo/rnn.py
+++ week13/labs/ts_demo/rnn.py
@@ -44,23 +44,25 @@ batch[:,-1:]
 batchdf = batch.squeeze(-1).numpy().transpose()
 pd.DataFrame(batchdf[::-1]).plot()
 
-
-# Create a mask so we can tell the model where our padding is
-mask = batch == -1
-pd.DataFrame(mask[:,:,0].numpy().transpose())
-
-# First lets normalise our data
-dmean = batch[~mask].mean()
-dstd = batch[~mask].std()
-
-batch[~mask] = (batch[~mask] - dmean) / dstd
-
-batchdf = batch.squeeze(-1).numpy().transpose()
-pd.DataFrame(batchdf[::-1]).plot()
-
 # Split the data into our target and actuals
 yact = batch[:, :5]
 x = batch[:, 5:]
+
+
+# Create a mask so we can tell the model where our padding is
+mask = x == -1
+pd.DataFrame(x[:,:,0].numpy().transpose())
+
+# First lets normalise our data
+dmean = x[~mask].mean()
+dstd = x[~mask].std()
+
+x[~mask] = (x[~mask] - dmean) / dstd
+
+xdf = x.squeeze(-1).numpy().transpose()
+pd.DataFrame(xdf[::-1]).plot()
+ydf = yact.squeeze(-1).numpy().transpose()
+pd.DataFrame(ydf[::-1]).plot()
 
 
 
@@ -132,10 +134,89 @@ class TimeSeriesRNN(nn.Module):
         
         return out
 
-# self = model = TimeSeriesRNN(input_feats = 1, outlen = yact.size(1), hidden = 32)
 model = TimeSeriesRNN(input_feats = 1, outlen = yact.size(1), hidden = 32)
 model
 
 ypred = model(batch)
 loss = criterion(ypred, yact)
 loss
+
+# Lets add categories to our data
+PADCATSVALUE = 0
+xcats = torch.randint(1,5, (x.shape))
+xcats[mask] = PADCATSVALUE
+pd.DataFrame(xcats.squeeze(-1).numpy().transpose())
+
+xinp = torch.cat((x, xcats), -1)
+
+xinp.shape
+
+class TimeSeriesRNN(nn.Module):
+    def __init__(self, input_conts, outlen, hidden_dim = 32):
+        super().__init__()
+        self.rnn = nn.LSTM(input_size=input_conts+hidden_dim,
+                           hidden_size=hidden_dim,
+                           batch_first=True, 
+                           bidirectional = False)
+        self.fc = nn.Linear(in_features=hidden_dim ,out_features=1)
+        self.bn = nn.BatchNorm1d(num_features=1)
+        self.embed = nn.Embedding(5, hidden_dim)
+        self.outlen = outlen
+        self.contidx = 0
+        self.catidx = 1
+
+    def forward(self, xinp):
+
+        xcont = xinp [:,:, [self.contidx]] .permute(0 ,2, 1)
+        xcont =  self.bn(xcont)
+        xcont = xcont.permute(0 ,2, 1)
+        
+        xcat = xinp[:,:, self.catidx].long()
+        xemb = self.embed(xcat)
+        
+        x = torch.cat((xcont, xemb), -1)
+        
+        hidden, _ = self.rnn(x)
+        hidden = hidden[:,:self.outlen]
+        
+        out = self.fc(hidden)
+        
+        return out
+
+self = model = TimeSeriesRNN( 1, yact.size(1),  32)
+ypred = model(xinp)
+loss = criterion(ypred, yact)
+loss
+
+
+
+# Transformers
+mha = torch.nn.MultiheadAttention(embed_dim = 32, num_heads = 4)
+embed = nn.Embedding(5, hidden_dim)
+fc = nn.Linear(in_features=hidden_dim ,out_features=1)
+
+xcats = xinp[:, :, 1].long()
+xemb = embed(xcats)
+# https://pytorch.org/docs/stable/generated/torch.nn.MultiheadAttention.html
+'''
+Shapes for inputs:
+query: (L, N, E)(L,N,E) where L is the target sequence length, N is the batch size, E is the embedding dimension.
+key: (S, N, E)(S,N,E) , where S is the source sequence length, N is the batch size, E is the embedding dimension.
+value: (S, N, E)(S,N,E) where S is the source sequence length, N is the batch size, E is the embedding dimension.
+'''
+xemb.shape
+xemb = xemb.permute(1, 0, 2)
+
+attn_output, attn_output_weights = mha(xemb[:5], xemb, xemb)
+
+'''
+Shapes for outputs:
+attn_output: (L, N, E)(L,N,E) where L is the target sequence length, N is the batch size, E is the embedding dimension.
+attn_output_weights: (N, L, S)(N,L,S) where N is the batch size, L is the target sequence length, S is the source sequence length.
+'''
+attn_output.shape
+attn_output = attn_output.permute(1,0,2)
+ypred = fc(attn_output)
+loss = criterion(ypred, yact)
+loss
+
